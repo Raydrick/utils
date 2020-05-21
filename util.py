b3f6f64a2d41e52320c20c1ab4588427f2a1f74f
Raydrick <32701726+Raydrick@users.noreply.github.com>
2020-05-22 00:38:55 +0200

    creating util.py and first methods

---
+++ util.py
@@ -0,0 +1,37 @@
+#constants
+INT = int
+STR = str
+FLOAT = float
+
+def isint(var):
+    try:
+        i = int(var)
+        return True
+    except ValueError:
+        return False
+def isdecimal(var):
+    try:
+        i = float(var)
+        return True
+    except ValueError:
+        return False
+
+def rinput(msg,valuetype):
+    if valuetype == str:
+        return input(msg)
+    elif valuetype == int:
+        while True:
+            inp = input(msg)
+            if isint(inp):
+                return int(inp)
+            print("Input was not valid. Try again!")
+    elif valuetype == float:
+        while True:
+            inp = input(msg)
+            inp = inp.replace(",",".")
+            if isdecimal(inp):
+                return float(inp)
+            print("Input was not valid. Try again!")
+    else:
+        print("Wrong 'valuetype'. Use 'STR' or 'INT'")
+
